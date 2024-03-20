98105d4cee30421ffe56e28082b4dd2b336777f8
Scott Archer-Nicholls <s.archer-nicholls@gmail.com>
2024-03-20 10:46:44 +0000

    Refactoring

--- catchment-analysis.py
+++ catchment-analysis.py
@@ -17,21 +17,40 @@ def main(args):
     InFiles = args.infiles
     if not isinstance(InFiles, list):
         InFiles = [args.infiles]
-    
+
     if args.full_data_analysis:
-        compute_data.analyse_data(os.path.dirname(InFiles[0]))
+        _, extension = os.path.splitext(InFiles[0])
+        if extension == '.json':
+            data_source = compute_data.JSONDataSource(os.path.dirname(InFiles[0]))
+        elif extension == '.csv':
+            data_source = compute_data.CSVDataSource(os.path.dirname(InFiles[0]))
+        else:
+            raise ValueError(f'Unsupported file format: {extension}')
+
+        daily_standard_deviation = compute_data.analyse_data(data_source)
+        graph_data = {
+            'daily standard deviation': daily_standard_deviation
+        }
+
+        views.visualize(graph_data)
 
     for filename in InFiles:
         measurement_data = models.read_variable_from_csv(filename)
-        
-        view_data = {'daily sum': models.daily_total(measurement_data), 'daily average': models.daily_mean(measurement_data), 'daily max': models.daily_max(measurement_data), 'daily min': models.daily_min(measurement_data)}
-        
+
+        view_data = {
+            'daily sum': models.daily_total(measurement_data),
+            'daily average': models.daily_mean(measurement_data),
+            'daily max': models.daily_max(measurement_data),
+            'daily min': models.daily_min(measurement_data)
+        }
+
         views.visualize(view_data)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='A basic environmental data management system')
-    
+
     parser.add_argument(
         'infiles',
         nargs='+',
@@ -40,5 +59,5 @@ if __name__ == "__main__":
     parser.add_argument('--full-data-analysis', action='store_true', dest='full_data_analysis')
     
     args = parser.parse_args()
-    
+
     main(args)
